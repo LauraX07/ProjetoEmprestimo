34f1489fa4d8f39353f8a7f26d1cb826104b75cb
unknown <3mtec@e041edu.local>
2026-04-29 09:28:06 -0300

    Criando classe Cookie Carrinho de Compras

---
+++ ProjetoEmprestimo/CarrinhoCompra/CookieCarrinhoCompra.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using ProjetoEmprestimo.Models;
+
+namespace ProjetoEmprestimo.CarrinhoCompra
+{
+    public class CookieCarrinhoCompra
+    {
+        //criar uma chave
+        private string Key = "Carrinho.Compras";
+        private Cookie.Cookie _cookie;
+
+        public CookieCarrinhoCompra(Cookie.Cookie cookie)
+        {
+            _cookie = cookie;
+        }
+
+        //Salvar
+        public void Salvar(List<Livro> Lista)
+        {
+            string Valor = JsonConvert.SerializeObject(Lista);
+            _cookie.Cadastrar(Key, Valor);
+
+        }
+
+        //Consulta
+        public List<Livro> Consultar()
+        {
+            if (_cookie.Existe(Key))
+            {
+                string valor = _cookie.Consultar(Key);
+                return JsonConvert.DeserializeObject<List<Livro>>(valor); 
+            }
+            else
+            {
+                return new List<Livro>();
+            }
+        }
+    }
+}
